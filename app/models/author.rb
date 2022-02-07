d911eac8a28823ab5d93045a967cb5b28eef8e42
Liamtran1997 <workwithtuan1908@gmail.com>
2022-02-07 16:06:47 +0700

    Validation Authors and posts

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,6 @@
 class Author < ApplicationRecord
   has_many :posts
+  validates :username, uniqueness: true, presence: true ,length: {in: 4..12}
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, length: {in: 6..16}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author ,dependent: :destroy
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true
+  validates :author_id, presence: true
 end
